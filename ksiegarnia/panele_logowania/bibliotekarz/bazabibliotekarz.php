8a9e7fcaaae0e64187baa7d301491bdc7de079a5
mxvoa <124454688+mxvoa@users.noreply.github.com>
2023-06-27 18:52:12 +0200

    Delete ksiegarnia directory

--- ksiegarnia/panele_logowania/bibliotekarz/bazabibliotekarz.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-    $host = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "bibliotekarz";
-?>
--- ksiegarnia/panele_logowania/uzytkownik/baza.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $host = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "klient";
-?>
