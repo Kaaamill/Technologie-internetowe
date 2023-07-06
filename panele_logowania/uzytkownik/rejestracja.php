cc5b617839ef1456f64fe2e09e5c1205e5064496
peleda <orwat.am@gmail.com>
2023-07-06 14:12:08 +0200

    fix rejestracja.php

--- panele_logowania/uzytkownik/rejestracja.php
+++ panele_logowania/uzytkownik/rejestracja.php
@@ -121,6 +121,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Rejestracja dla Klienta</title>
+    <link rel="stylesheet" href="../../css_styles/rejestracja.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
      
     <style>
         .error
@@ -133,7 +135,10 @@
 </head>
  
 <body>
-     
+<div class="container">
+<div id="regulamin"></div>
+    <a href="../../index.html"><img class="logo " src="../../img/logo.png" style="border-radius: 25px; opacity: 95%;" /></a>
+    <div id="box">
     <form method="post">
      
         Login: <br /> <input type="text" value="<?php 
@@ -214,5 +219,33 @@
         <input type="submit" value="Zarejestruj się" />
          
     </form>
+    </div>
+    <footer>
+        <nav class="nav">
+            <h4 class="sm-header">Śledź nas na</h4>
+            <div class="line"></div>
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="https://www.facebook.com/collegiumdavinci" target ="_blank" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://twitter.com/CDV_Poznan" target ="_blank" class="nav-link"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://www.instagram.com/collegium_da_vinci/" target ="_blank" class="nav-link"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://cdv.pl" target ="_blank" class="nav-link"><i class="fab fa-google"></i></a>
+                </li>
+            </ul>
+        </nav>
+        Bilbioteka Publiczna VaDinci<br />
+        VaDinci@gmail.com<br />
+        ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
+        &copy; 2023 VaDinci
+
+    </footer>
+    </div>
+    <script src="../../js_scripts/regulaminmodal.js"></script>
 </body>
 </html>
