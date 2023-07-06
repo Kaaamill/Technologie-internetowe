b1ba5d8ba26e6519279a17e666a7c8cf342dfe8f
peleda <orwat.am@gmail.com>
2023-07-06 14:05:13 +0200

    html w "porejestracji"

--- panele_logowania/uzytkownik/porejestracji.php
+++ panele_logowania/uzytkownik/porejestracji.php
@@ -4,7 +4,7 @@
      
     if (!isset($_SESSION['udanarejestracja']))
     {
-        header('Location: ../../index.html');
+        header('Location: index.php');
         exit();
     }
     else
@@ -22,6 +22,7 @@
     if (isset($_SESSION['e_email'])) unset($_SESSION['e_email']);
     if (isset($_SESSION['e_haslo2'])) unset($_SESSION['e_haslo2']);
     if (isset($_SESSION['e_regulamin'])) unset($_SESSION['e_regulamin']);
+    if (isset($_SESSION['e_bot'])) unset($_SESSION['e_bot']);
      
 ?>
 
@@ -30,13 +31,45 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <link rel="stylesheet" href="../../css_styles/rejestracja.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
     <title>Udana rejestracja!</title>
 </head>
 <body>
-    Dziękujemy za rejestrację w naszym serwisie!<br /><br />
-     
-    <a href="../../index.html">Zaloguj się na swoje konto i zapraszamy do wypożyczania książek z naszej Biblioteki VaDinci!</a>
-    <br /><br />
- 
+<div class="container">
+    <a href="../../index.html"><img class="logo " src="../../img/logo.png" style="border-radius: 25px; opacity: 95%;" /></a>
+    <div id="rejestracja-box">
+        Dziękujemy za rejestrację w naszym serwisie!<br /><br />
+        <div id="porejestracji">
+            <a href="../../index.html">Zaloguj się na swoje konto i zapraszamy do wypożyczania książek z naszej Biblioteki VaDinci!</a>
+            <br /><br />
+        </div>
+    </div>
+    <footer>
+        <nav class="nav">
+            <h4 class="sm-header">Śledź nas na</h4>
+            <div class="line"></div>
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="https://www.facebook.com/collegiumdavinci" target ="_blank" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://twitter.com/CDV_Poznan" target ="_blank" class="nav-link"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://www.instagram.com/collegium_da_vinci/" target ="_blank" class="nav-link"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://cdv.pl" target ="_blank" class="nav-link"><i class="fab fa-google"></i></a>
+                </li>
+            </ul>
+        </nav>
+        Bilbioteka Publiczna VaDinci<br />
+        VaDinci@gmail.com<br />
+        ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
+        &copy; 2023 VaDinci
+
+    </footer>
+</div>
 </body>
 </html>
