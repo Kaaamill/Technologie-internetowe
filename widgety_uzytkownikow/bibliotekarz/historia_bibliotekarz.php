cb0b9ed4f5593c4e833016473e41ede5ce37ceb0
DominikDobski <dominik.k.dobski@gmail.com>
2023-07-07 21:54:32 +0200

    wysiwetlanie

--- widgety_uzytkownikow/bibliotekarz/historia_bibliotekarz.php
+++ widgety_uzytkownikow/bibliotekarz/historia_bibliotekarz.php
@@ -1,15 +1,18 @@
-﻿<?php
-    session_start();
-    if (!isset($_SESSION['zalogowany']))
-    {
-        header('Location: ../../index.html');
-        exit();
-    }
+﻿﻿<?php
+session_start();
+if (!isset($_SESSION['zalogowany'])) {
+    header('Location: ../../index.html');
+    exit();
+}
 ?>
 
+<?php
+include "baza.php";
+?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,71 +22,100 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
 
 </head>
-<body>
-<div class="container">
-    <div class="main">
-        <a href="historia_bibliotekarz.php"><img class="logo " src="../../img/logo.png" style="border-radius: 25px; opacity: 95%;" /></a>
-        <nav class="menu">
-            <ul>
-                <li><a href="strona_glowna_bibliotekarz.php">Strona Główna</a></li>
-                <li><a href="ksiazki_bibliotekarz.php">Książki</a></li>
-                <li><a href="uzytkownicy_bibliotekarz.php">Użytkownicy</a></li>
-                <li  class="special"><a href="historia_bibliotekarz.php">Historia</a></li>
-                <li><a href="wylogowanie.php">Wyloguj</a></li>
-            </ul>
-        </nav>
-        <div id="main_historia">
-            <h1>HISTORIA</h1>
-            <div id="tabela_historia">
-                <table>
-                    <tr>
-                        <th>Tytuł książki</th>
-                        <th>Kto wypożyczył</th>
-                        <th>Data wypożyczenia</th>
-                        <th>Data zwrotu</th>
-                    </tr>
-                    <tr>
-                        <td>Dane 1</td>
-                        <td>Dane 2</td>
-                        <td>Dane 3</td>
-                        <td>Dane 3</td>
-                    </tr>
-                    <tr>
-                        <td>Dane 4</td>
-                        <td>Dane 5</td>
-                        <td>Dane 6</td>
-                        <td>Dane 3</td>
-                    </tr>
-                </table>
 
+<body>
+    <div class="container">
+        <div class="main">
+            <a href="strona_glowna_bibliotekarz.php"><img class="logo " src="../../img/logo.png"
+                    style="border-radius: 25px; opacity: 95%;" /></a>
+            <nav class="menu">
+                <ul>
+                    <li><a href="strona_glowna_bibliotekarz.php">Strona Główna</a></li>
+                    <li><a href="ksiazki_bibliotekarz.php">Książki</a></li>
+                    <li><a href="rezerwacje_bibliotekarz.php">Rezerwacje</a></li>
+                    <li class="special"><a href="historia_bibliotekarz.php">Historia</a></li>
+                    <li><a href="wylogowanie.php">Wyloguj</a></li>
+                </ul>
+            </nav>
+            <div id="main_historia">
+                <h1>HISTORIA WYPOŻYCZEŃ</h1>
+                <div id="tabela_historia">
+                    <table>
+                        <tr>
+                            <th>TYTUŁ KSIĄŻKI</th>
+                            <th>REZERWACJA</th>
+                            <th>CZY WYPOŻYCZONA</th>
+                            <th>DATA WYPOŻYCZENIA</th>
+                            <th>ZWRÓCONA</th>
+                            <th>DATA ZWROTU</th>
+                            <th>LOGIN OSOBY WYPOŻYCZAJĄCEJ</th>
+                        </tr>
+                        <?php
+                        $sql = "SELECT * FROM `id_ksiazki`";
+                        $result = mysqli_query($conn, $sql);
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                            <tr>
+                                <td>
+                                    <?php echo $row["tytul_ksiazki"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["rezerwacja"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["czy_wypozyczona"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["data_wypozyczenia"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["czy_zwrocona"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["data_zwrotu"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["login"] ?>
+                                </td>
+                            </tr>
+
+                            <?php
+                        }
+                        ?>
+                    </table>
+
+                </div>
             </div>
         </div>
-    </div>
-    <footer>
-        <nav class="nav">
-            <h4 class="sm-header">Śledź nas na</h4>
-            <div class="line"></div>
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <a href="https://www.facebook.com/collegiumdavinci" target ="_blank" class="nav-link"><i class="fab fa-facebook-f"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://twitter.com/CDV_Poznan" target ="_blank" class="nav-link"><i class="fab fa-twitter"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://www.instagram.com/collegium_da_vinci/" target ="_blank" class="nav-link"><i class="fab fa-instagram"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://cdv.pl" target ="_blank" class="nav-link"><i class="fab fa-google"></i></a>
-                </li>
-            </ul>
-        </nav>
-        Bilbioteka Publiczna VaDinci<br />
-        VaDinci@gmail.com<br />
-        ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
-        &copy; 2023 VaDinci
+        <footer>
+            <nav class="nav">
+                <h4 class="sm-header">Śledź nas na</h4>
+                <div class="line"></div>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com/collegiumdavinci" target="_blank" class="nav-link"><i
+                                class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://twitter.com/CDV_Poznan" target="_blank" class="nav-link"><i
+                                class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.instagram.com/collegium_da_vinci/" target="_blank" class="nav-link"><i
+                                class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://cdv.pl" target="_blank" class="nav-link"><i class="fab fa-google"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            Bilbioteka Publiczna VaDinci<br />
+            VaDinci@gmail.com<br />
+            ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
+            &copy; 2023 VaDinci
 
-</footer>
-</div>
+        </footer>
+    </div>
 </body>
+
 </html>
--- widgety_uzytkownikow/uzytkownik/strona_glowna_uzytkownik.php
+++ widgety_uzytkownikow/uzytkownik/strona_glowna_uzytkownik.php
@@ -5,52 +5,19 @@ if (!isset($_SESSION['zalogowany'])) {
     exit();
 }
 
+// Obsługa formularza wyszukiwania
+if (isset($_GET['fraza'])) {
+    $fraza = $_GET['fraza'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $searchText = $_POST['searchText'];
-
-    // Przetwarzanie wyszukiwania
-    if (mb_stripos($searchText, 'Wlad', 0, 'UTF-8') !== false) {
-        header("Location: podstrony_ksiazki/k1.php");
-        exit;
-    } elseif (mb_stripos($searchText, 'Harry', 0, 'UTF-8') !== false) {
-        header("Location: podstrony_ksiazki/k2.php");
-        exit;
-        } elseif (mb_stripos($searchText, 'gra', 0, 'UTF-8') !== false) {
-                header("Location: podstrony_ksiazki/k3.php");
-                exit;
-                } elseif (mb_stripos($searchText, 'miecz', 0, 'UTF-8') !== false) {
-                        header("Location: podstrony_ksiazki/k4.php");
-                        exit;
-                        } elseif (mb_stripos($searchText, 'oskar', 0, 'UTF-8') !== false) {
-                                header("Location: podstrony_ksiazki/k5.php");
-                                exit;
-                                } elseif (mb_stripos($searchText, 'zbrodnia') !== false) {
-                                        header("Location: podstrony_ksiazki/k6.php");
-                                        exit;
-                                        } elseif (mb_stripos($searchText, 'krew', 0, 'UTF-8') !== false) {
-                                                header("Location: podstrony_ksiazki/k7.php");
-                                                exit;} elseif (mb_stripos($searchText, 'igrzys', 0, 'UTF-8') !== false) {
-                                                             header("Location: podstrony_ksiazki/k9.php");
-                                                             exit;
-                                                             } elseif (mb_stripos($searchText, 'zmie', 0, 'UTF-8') !== false) {
-                                                                     header("Location: podstrony_ksiazki/k10.php");
-                                                                     exit;
-                                                                     } elseif (mb_stripos($searchText, 'ostatn', 0, 'UTF-8') !== false) {
-                                                                             header("Location: podstrony_ksiazki/k11.php");
-                                                                             exit;
-
-
-    } else {
-        // Obsługa innych przypadków
-        echo "Brak wyników dla wyszukiwania: $searchText";
-        exit;
-    }
+    // Przekierowanie na stronę z wynikami wyszukiwania
+    header("Location: wyniki_wyszukiwania.php?fraza=$fraza");
+    exit();
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -60,98 +27,159 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
 
 </head>
+
 <body>
-<div class="container">
-    <div class="main">
-        <a href="strona_glowna_uzytkownik.php"><img class="logo " src="../../img/logo.png" style="border-radius: 25px; opacity: 95%;" /></a>
-        <nav class="menu">
-            <ul>
-                <li class="special"><a href="strona_glowna_uzytkownik.php">Strona Główna</a></li>
-                <li><a href="ksiazki_uzytkownik.php">Książki</a></li>
-                <li><a href="historia_uzytkownik.php">Historia</a></li>
-                <li><a href="wylogowanie.php">Wyloguj</a></li>
-            </ul>
-        </nav>
-    </div>
-    <h1>
-        <section>
-            <form action="strona_glowna_uzytkownik.php" method="POST">
-              <input type="search" name="searchText" placeholder="Wyszukaj książki" required>
-              <input type="submit" value="Szukaj">
+    <div class="container">
+        <div class="main">
+            <a href="strona_glowna_uzytkownik.php"><img class="logo " src="../../img/logo.png"
+                    style="border-radius: 25px; opacity: 95%;" /></a>
+            <nav class="menu">
+                <ul>
+                    <li class="special"><a href="strona_glowna_uzytkownik.php">Strona Główna</a></li>
+                    <li><a href="ksiazki_uzytkownik.php">Książki</a></li>
+                    <li><a href="historia_uzytkownik.php">Historia</a></li>
+                    <li><a href="wylogowanie.php">Wyloguj</a></li>
+                </ul>
+            </nav>
+        </div>
+        <h1>
+            <form action="strona_glowna_uzytkownik.php" method="GET">
+                <input type="text" name="fraza" placeholdeimgr="Wyszukaj ksiazke...">
+                <button type="submit">Szukaj</button>
             </form>
-          </section>
-    </h1>
-    <h1>POLECAMY:</h1>
+        </h1>
+        <h1>POLECAMY:</h1>
 
 
-     <div class="books-container" style="display: flex; justify-content: center;">
+        <div class="container_2">
 
-          <div class="pierwsza">
-              <a href="podstrony_ksiazki/k1.php" target="_blank" title="Zobacz recenzje">
-                  <div class="image-container">   <img src="../../img//wp.jpg" width="100"> <a href="Wypozyczenie.php" class="rent-button">Wypożycz</a> </div>
-              </a><br>
-              <a href="podstrony_ksiazki/k1.php" target="_blank" title="Zobacz recenzje">Władca Pierścieni</a><br/><br/> </div>
+            <br>
+            <div class="druga">
+                <a href="podstrony_ksiazki/k1.php" target="_blank" title="Zobacz recenzje">
+                    <div class="image-container"> <img src="../../img/wp.jpg" width="120"> <a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a> </div>
+                </a><br>
+                <a href="podstrony_ksiazki/k1.php" target="_blank" title="Zobacz recenzje">Władca
+                    Pierścieni</a><br /><br />
+            </div>
 
-          <div class="pierwsza">
-              <a href="podstrony_ksiazki/k2.php" target="_blank" title="Zobacz recenzje">
-                  <div class="image-container"> <img width="100" src="../../img/hpkf.jpg" /> <a href="Wypozyczenie.php" class="rent-button">Wypożycz</a>
-                  </div>
-              </a><br/>
-              <a href="podstrony_ksiazki/k2.php" target="_blank" title="Zobacz recenzje">Harry Potter i Kamień Filozoficzny</a>
-          </div>
-          <br/>
-          <div class="pierwsza">
-              <a href="podstrony_ksiazki/k9.php" target="_blank" title="Igrzyska śmierci">
-                  <div class="image-container">   <img width="100" src="../../img/9.jpg" /><a href="Wypozyczenie.php" class="rent-button">Wypożycz</a>
-                  </div>
-              </a><br/>
-              <a href="podstrony_ksiazki/k9.php" target="_blank" title="Zobacz recenzje">Igrzyska śmierci</a>
-          </div>
+            <div class="druga">
 
-          <div class="pierwsza">
-              <a href="podstrony_ksiazki/k10.php" target="_blank" title="Zmierzch">
-                  <div class="image-container">   <img width="100" src="../../img/10.jpg" /><a href="Wypozyczenie.php" class="rent-button">Wypożycz</a>
-                  </div>
-              </a><br/>
-              <a href="podstrony_ksiazki/k10.php" target="_blank" title="Zobacz recenzje">Zmierzch</a>
-          </div>
-          <div class="pierwsza">
-              <a href="podstrony_ksiazki/k11.php" target="_blank" title="Ostatnie życzenie">
-                  <div class="image-container">   <img width="100" src="../../img/11.jpg" /><a href="Wypozyczenie.php" class="rent-button">Wypożycz</a>
-                  </div>
-              </a><br/>
-              <a href="podstrony_ksiazki/k11.php" target="_blank" title="Zobacz recenzje">Ostatnie życzenie</a>
-          </div>
+                <a href="podstrony_ksiazki/k2.php" target="_blank" title="Zobacz recenzje">
+                    <div class="image-container"> <img width="120" src="../../img/hpkf.jpg" /> <a
+                            href="Wypozyczenie.php" class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k2.php" target="_blank" title="Zobacz recenzje">Harry Potter i Kamień
+                    Filozoficzny</a>
 
-      </div>
-      </div>
- <h1><a href="ksiazki_uzytkownik.php" target="_blank" class="button">Zobacz więcej pozycji</a></h1>
+            </div>
 
-<footer>
-        <nav class="nav">
-            <h4 class="sm-header">Śledź nas na</h4>
-            <div class="line"></div>
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <a href="https://www.facebook.com/collegiumdavinci" target ="_blank" class="nav-link"><i class="fab fa-facebook-f"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://twitter.com/CDV_Poznan" target ="_blank" class="nav-link"><i class="fab fa-twitter"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://www.instagram.com/collegium_da_vinci/" target ="_blank" class="nav-link"><i class="fab fa-instagram"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://cdv.pl" target ="_blank" class="nav-link"><i class="fab fa-google"></i></a>
-                </li>
-            </ul>
-        </nav>
-        Bilbioteka Publiczna VaDinci<br />
-        VaDinci@gmail.com<br />
-        ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
-        &copy; 2023 VaDinci
 
-</footer>
-</div>
+            <div class="druga">
+                <a href="podstrony_ksiazki/k3.php" target="_blank" title="Gra o tron (edycja ilustrowana)">
+                    <div class="image-container"> <img width="120" src="../../img/3.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k3.php" target="_blank" title="Zobacz recenzje">Gra o tron (edycja
+                    ilustrowana)</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k4.php" target="_blank" title="Miecz przeznaczenia">
+                    <div class="image-container"> <img width="120" src="../../img/4.jpg" /> <a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k4.php" target="_blank" title="Zobacz recenzje">Miecz przeznaczenia</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k5.php" target="_blank" title="Oskar i pani Róża">
+                    <div class="image-container"> <img width="120" src="../../img/5.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k5.php" target="_blank" title="Zobacz recenzje">Oskar i pani Róża</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k6.php" target="_blank" title="Zbrodnia i kara">
+                    <div class="image-container"> <img width="120" src="../../img/6.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k6.php" target="_blank" title="Zobacz recenzje">Zbrodnia i kara</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k7.php" target="_blank" title="Krew elfów">
+                    <div class="image-container"> <img width="120" src="../../img/7.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k7.php" target="_blank" title="Zobacz recenzje">Krew elfów</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k9.php" target="_blank" title="Igrzyska śmierci">
+                    <div class="image-container"> <img width="120" src="../../img/9.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k9.php" target="_blank" title="Zobacz recenzje">Igrzyska śmierci</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k10.php" target="_blank" title="Zmierzch">
+                    <div class="image-container"> <img width="120" src="../../img/10.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k10.php" target="_blank" title="Zobacz recenzje">Zmierzch</a>
+            </div>
+
+            <div class="druga">
+                <a href="podstrony_ksiazki/k11.php" target="_blank" title="Ostatnie życzenie">
+                    <div class="image-container"> <img width="120" src="../../img/11.jpg" /><a href="Wypozyczenie.php"
+                            class="rent-button">Wypożycz</a>
+                    </div>
+                </a><br />
+                <a href="podstrony_ksiazki/k11.php" target="_blank" title="Zobacz recenzje">Ostatnie życzenie</a>
+            </div>
+        </div>
+        <h1><a href="ksiazki_uzytkownik.php" target="_blank" class="button">Zobacz więcej pozycji</a></h1>
+
+        <footer>
+            <nav class="nav">
+                <h4 class="sm-header">Śledź nas na</h4>
+                <div class="line"></div>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com/collegiumdavinci" target="_blank" class="nav-link"><i
+                                class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://twitter.com/CDV_Poznan" target="_blank" class="nav-link"><i
+                                class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.instagram.com/collegium_da_vinci/" target="_blank" class="nav-link"><i
+                                class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://cdv.pl" target="_blank" class="nav-link"><i class="fab fa-google"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            Bilbioteka Publiczna VaDinci<br />
+            VaDinci@gmail.com<br />
+            ul. Tadeusza Kutrzeby 1, 61-710 Poznań<br />
+            &copy; 2023 VaDinci
+
+        </footer>
+    </div>
 </body>
+
 </html>
